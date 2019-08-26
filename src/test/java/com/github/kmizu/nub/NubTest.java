7ba6cc449860172b03a4792391442a57448481cd
Kota Mizushima <mizukota@gmail.com>
2019-08-27 01:11:35 +0900

    Add one test

--- src/test/java/com/github/kmizu/nub/NubTest.java
+++ src/test/java/com/github/kmizu/nub/NubTest.java
@@ -1,11 +1,27 @@
 package com.github.kmizu.nub;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import static com.github.kmizu.nub.AstNode.*;
 
 @RunWith(JUnit4.class)
 public class NubTest {
-    static Object eval(AstNode.ExpressionList input) throws Exception {
-        return new Evaluator().evaluate(input);
+    private static Object eval(Expression input) {
+        return new Evaluator().evaluate(
+                new ExpressionList(
+                        Arrays.asList(input)
+                )
+        );
     }
+
+    @Test
+    public void testStringLiteral() {
+        assertEquals("Hello", eval(new StringLiteral("Hello")));
+    }
+
 }
