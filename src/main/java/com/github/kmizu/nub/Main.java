f22d2383d2ad9d8ed727504acd027ca21b073c00
Kota Mizushima <mizukota@gmail.com>
2019-08-26 15:24:33 +0900

    Resolve compilation problem

--- src/main/java/com/github/kmizu/nub/Main.java
+++ src/main/java/com/github/kmizu/nub/Main.java
@@ -1,8 +1,7 @@
 package com.github.kmizu.nub;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
+        System.out.println("nub2");
     }
 }
--- src/test/java/com/github/kmizu/nub/NubTest.java
+++ src/test/java/com/github/kmizu/nub/NubTest.java
@@ -5,7 +5,7 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class NubTest {
-    static Object eval(ASTNode input) throws Exception {
-        return new Evaluator().evaluate(new NubParser(Streams.streamFrom(input)).program().e);
+    static Object eval(AstNode.ExpressionList input) throws Exception {
+        return new Evaluator().evaluate(input);
     }
 }
