74e712a8d637489293e443683c0721670e877d99
Kota Mizushima <mizukota@gmail.com>
2019-11-08 17:59:24 +0900

    Some refactorings

--- src/main/java/com/github/kmizu/nub2/Main.java
+++ src/main/java/com/github/kmizu/nub2/Main.java
@@ -1,15 +1,14 @@
 package com.github.kmizu.nub2;
 
 import static com.github.kmizu.nub2.Ast.*;
+import static com.github.kmizu.nub2.Ast.Factory.*;
 
 public class Main {
     public static void main(String[] args) {
         Evaluator evaluator = new Evaluator();
         evaluator.eval(
-                new Block(
-                        new PrintlnExpression(
-                                new StringLiteral("Hello, World")
-                        )
+                tBlock(
+                        tPrintln(tString("Hello, World"))
                 )
         );
     }
